56dba1c45c76046dff9f17e1f157326a20ac1e3e
itsmesauravk <np03cs4s230139@heraldcollege.edu.np>
2023-05-27 19:40:19 +0545

    modified

---
+++ js/calcualtion.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+
+number = () => {
+    const num1 = prompt("Enter the first number : ")
+    const num2 = prompt("Enter the ssecond number : ")
+    result = num1 + num2
+    return result
+}
+number() 
